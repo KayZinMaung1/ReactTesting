4773469a99c29cb03562d67d8ac675164b667111
KayZinMaung1 <kayzinmg363@gmail.com>
2022-02-21 10:29:06 +0630

    useEffect

--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useState } from "react";
 import BlogList from "./BlogList";
 
@@ -9,18 +10,25 @@ const Home = () => {
     ]     
     )
 
+    const [name, setName] = useState('Mario');
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setblogs(newBlogs);
     }
-     
     
+    useEffect(() => {
+        console.log("UseEffect Run");
+        console.log({blogs});
+    },[name])
+
     return (  
-       
         <div className="home">
            <BlogList blogs={blogs} title='All Blogs' handleDelete={handleDelete}/>
+           <button onClick={() => setName('Luigi')}>Change Name</button>
+           <p>{name}</p>
         </div>
     );
 }
- 
+
 export default Home;
